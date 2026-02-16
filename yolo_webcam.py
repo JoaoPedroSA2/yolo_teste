01ade0961a13ac6fee6c56a363dbde7dd72b89c1
Joao Pedro <jpsa101106@gmail.com>
2026-02-16 15:35:27 -0300

    arrumando funcao de fala

--- yolo_webcam.py
+++ yolo_webcam.py
@@ -1,36 +1,47 @@
 import cv2
 from ultralytics import YOLO
 import winsound
-import pyttsx3
 import time
+import os
+import threading
 
 model = YOLO("yolov8n.pt")
 
+#-----configurações da webcam------
 cap = cv2.VideoCapture(0)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 540)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 680)
-
-engine = pyttsx3.init()
-
-frequencia = 1000
-duracao = 100
+#---------------------
 
 area_alerta = 40000
 
+#sistema alerta e cooldown
+ultimo_alerta = 0
+cooldown_alerta = 3 
+#--------
+
+def falar(texto):
+    def run(): #Função para rodar a fala em uma thread separada
+        comando = f'powershell -Command "Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak(\'{texto}\');"'
+        os.system(comando)
+
+    threading.Thread(target=run,daemon=True).start() #Inicia a thread para falar o texto
+
+# Loop principal
 while True:
-    ret, frame = cap.read()
+    ret, frame = cap.read() #Lê um frame da webcam
     if not ret:
         break
 
-    results = model(frame)
+    results = model(frame) #Realiza detecção no frame
 
-    if len(results[0].boxes) > 0:
+    if len(results[0].boxes) > 0:  #Verifica se há objetos detectados
         boxes = results[0].boxes.xyxy
         confs = results[0].boxes.conf
         classes = results[0].boxes.cls
-
-        index = int(confs.argmax())
+        
+        index = int(confs.argmax()) #Encontra o índice da caixa com a maior confiança
         obj = model.names[int(classes[index])]
         x1,y1,x2,y2 = boxes[index]
         comprimento = x2 - x1
@@ -38,13 +49,15 @@ while True:
 
         area = comprimento * altura
 
-        calculo_proximidade = 0.7 * altura + 0.3 * (area ** 0.5)
+        calculo_proximidade = 0.7 * altura + 0.3 * (area ** 0.5) #Cálculo de proximidade baseado na altura e área da caixa delimitadora
 
-        if calculo_proximidade > 300:
-            engine.say(f"Alerta! {obj} muito próximo!")
-            engine.runAndWait()
-            
-    frame_anotado = results[0].plot()
+        tempo_atual = time.time()
+
+        if calculo_proximidade > 300 and tempo_atual - ultimo_alerta > cooldown_alerta:  #Verifica se o objeto está próximo e se o cooldown do alerta passou
+            falar(f"Alerta: {obj} detectado próximo!")  #Fala o alerta  
+            ultimo_alerta = tempo_atual  #Atualiza o tempo do último alerta
+
+    frame_anotado = results[0].plot() #Desenha as caixas delimitadoras e rótulos no frame
 
     cv2.imshow("YOLO Webcam", frame_anotado)
 
