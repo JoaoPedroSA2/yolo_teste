d08eaa72ed6d9782273352047ca294798e2153f1
Joao Pedro <jpsa101106@gmail.com>
2026-03-08 11:51:31 -0300

    Arrumando problema de sobreposicao de fala

--- funcoes.py
+++ funcoes.py
@@ -1,6 +1,14 @@
 import threading
 import os
+import pytesseract
+import cv2
+import win32com.client 
 
+speaker = win32com.client.Dispatch("SAPI.SpVoice") #Configura o mecanismo de fala do Windows
+lock_fala = threading.Lock() #Cria um lock para controlar o acesso à fala, evitando sobreposição de falas
+
+caminho_pytesseract = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe" #Caminho para o executável do Tesseract OCR
+pytesseract.pytesseract.tesseract_cmd = caminho_pytesseract
 
 def nivel_distancia(valor):
     if valor > 400:
@@ -10,8 +18,8 @@ def nivel_distancia(valor):
     
 def falar(texto):
     def run(): #Função para rodar a fala em uma thread separada
-        comando = f'powershell -Command "Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak(\'{texto}\');"'
-        os.system(comando)
+        with lock_fala: #Adquire o lock para garantir que apenas uma fala ocorra por vez
+            speaker.Speak(texto) #Fala o texto usando o mecanismo de fala do Windows
 
     threading.Thread(target=run,daemon=True).start() #Inicia a thread para falar o texto
 
@@ -59,3 +67,21 @@ def objeto_prioritario(boxes, classes, model, centro_tela, largura_frame):
     mais_perto = max(objeto_detectado, key=lambda x: x["proximidade"])
     return mais_perto #Retorna o objeto mais próximo com base na proximidade calculada
 
+def extrair_texto_imagem(frame):
+   
+   imagem = frame
+
+   cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY) #Converte a imagem para escala de cinza para melhorar a precisão do OCR
+   cinza = cv2.convertScaleAbs(cinza, alpha=1, beta=8) #Ajusta o contraste e brilho da imagem para melhorar a legibilidade do texto
+   cinza = cv2.GaussianBlur(cinza, (3, 3), 0) #Aplica um desfoque gaussiano para reduzir o ruído na imagem
+   
+   _, thresh = cv2.threshold(cinza, 150,255,cv2.THRESH_BINARY) #Aplica uma limiarização para destacar o texto na imagem
+   
+   texto = pytesseract.image_to_string(thresh, config='--psm 6') #Extrai o texto da imagem usando o Tesseract
+   
+   cv2.imshow("Imagem Original", imagem) #Exibe a imagem original para referência
+   cv2.imshow("Texto Extraído", thresh) #Exibe a imagem processada para verificar o resultado do OCR
+
+   return texto.strip() #Retorna o texto extraído, removendo espaços em branco extras
+
+    
--- yolo_webcam.py
+++ yolo_webcam.py
@@ -7,7 +7,7 @@ from funcoes import *
 model = YOLO("yolov8n.pt")
 
 #-----configurações da webcam------
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 540)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 680)
@@ -25,10 +25,11 @@ largura_frame = 540
 centro_tela = largura_frame / 2
 #-----------------
 
-#Variáveis para armazenar o último objeto e direção detectados
+#Variáveis auliares
 ultimo_obj = None
 ultimo_direcao = None
 ultimo_nivel = None
+lendo_texto = False
 
 # Loop principal
 while True:
@@ -36,38 +37,59 @@ while True:
     if not ret:
         break
 
-    results = model(frame) #Realiza detecção no frame
+    if not lendo_texto: 
+        results = model(frame) #Realiza detecção no frame
 
-    if len(results[0].boxes) > 0:  #Verifica se há objetos detectados
-        boxes = results[0].boxes.xyxy
-        confs = results[0].boxes.conf
-        classes = results[0].boxes.cls
-        
-        prioritario = objeto_prioritario(boxes, classes, model, centro_tela, largura_frame) #Encontra o objeto prioritário
-
-        if prioritario:
-            tempo_atual = time.time()  #Obtém o tempo atual
-
-            #Armazena as informações do último objeto detectado para evitar alertas repetitivos
-            atual_obj = prioritario['obj']
-            atual_direcao = prioritario['direcao']
-            atual_nivel = prioritario['nivel'] 
-
-            mudou = (atual_obj != ultimo_obj or atual_direcao != ultimo_direcao or atual_nivel != ultimo_nivel) #Verifica se houve mudança no objeto, direção ou nível
-
-            if mudou and tempo_atual - ultimo_alerta > cooldown_alerta:
-                if atual_nivel == 1:
-                    falar(f"Alerta: {prioritario['obj']} muito próximo {prioritario['direcao']}")  #Fala o alerta para objetos muito próximos
-                    ultimo_alerta = tempo_atual  #Atualiza o tempo do último alerta
-                elif atual_nivel == 0:
-                    falar(f"Cuidado: {prioritario['obj']} perto {prioritario['direcao']}")  #Fala o alerta para objetos próximos
-                    ultimo_alerta = tempo_atual  #Atualiza o tempo do último alerta
+        if len(results[0].boxes) > 0:  #Verifica se há objetos detectados
+            boxes = results[0].boxes.xyxy
+            confs = results[0].boxes.conf
+            classes = results[0].boxes.cls
             
-                ultimo_obj = atual_obj
-                ultimo_direcao = atual_direcao
-                ultimo_nivel = atual_nivel
+            prioritario = objeto_prioritario(boxes, classes, model, centro_tela, largura_frame) #Encontra o objeto prioritário
 
-    frame_anotado = results[0].plot() #Desenha as caixas delimitadoras e rótulos no frame
+            if prioritario: 
+                tempo_atual = time.time()  #Obtém o tempo atual
+
+                #Armazena as informações do último objeto detectado para evitar alertas repetitivos
+                atual_obj = prioritario['obj']
+                atual_direcao = prioritario['direcao']
+                atual_nivel = prioritario['nivel'] 
+
+                mudou = (atual_obj != ultimo_obj or atual_direcao != ultimo_direcao or atual_nivel != ultimo_nivel) #Verifica se houve mudança no objeto, direção ou nível
+
+                if mudou and tempo_atual - ultimo_alerta > cooldown_alerta:
+                    if atual_nivel == 1:
+                        falar(f"Alerta: {prioritario['obj']} muito próximo {prioritario['direcao']}")  #Fala o alerta para objetos muito próximos
+                        ultimo_alerta = tempo_atual  #Atualiza o tempo do último alerta
+                    elif atual_nivel == 0:
+                        falar(f"Cuidado: {prioritario['obj']} perto {prioritario['direcao']}")  #Fala o alerta para objetos próximos
+                        ultimo_alerta = tempo_atual  #Atualiza o tempo do último alerta
+                
+                    ultimo_obj = atual_obj
+                    ultimo_direcao = atual_direcao
+                    ultimo_nivel = atual_nivel
+        
+    if cv2.waitKey(1) & 0xff == ord('l'): #Ativa a leitura de texto quando a tecla 'l' for pressionada
+        lendo_texto = True
+
+        speaker.Speak("",3)  #Limpa a fila de fala para evitar sobreposição de mensagens
+
+        falar("iniciando")  #Fala uma mensagem indicando que a leitura está sendo iniciada
+        print("Iniciando leitura")
+        texto_extraido = extrair_texto_imagem(frame)  #Extrai o texto do frame usando a função definida anteriormente
+        
+        if texto_extraido.strip():  #Verifica se o texto extraído não está vazio
+            print(f"Texto encontrado: {texto_extraido}")
+            falar(f"Texto encontrado {texto_extraido}")  #Fala o texto extraído da imagem
+        else:
+            print("Nenhum texto encontrado")  
+            falar("Erro ao ler texto")  #Fala uma mensagem de erro caso não seja possível extrair o texto
+        
+        lendo_texto = False  #Desativa a leitura de texto após a conclusão
+        ultimo_alerta = time.time() #Atualiza o tempo do último alerta para evitar alertas repetitivos após a leitura de texto
+
+    if not lendo_texto:
+        frame_anotado = results[0].plot() #Desenha as caixas delimitadoras e rótulos no frame
 
     cv2.imshow("YOLO Webcam", frame_anotado)
 
@@ -76,3 +98,4 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
